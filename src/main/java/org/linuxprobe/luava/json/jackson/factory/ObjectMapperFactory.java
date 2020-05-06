3ea2efdb5e267749460677d9cde3653456f707ee
larryyu <linuxprobe.org@gmail.com>
2020-05-06 18:50:40 +0800

    factory/ObjectMapperFactory.java update

--- src/main/java/org/linuxprobe/luava/json/jackson/factory/ObjectMapperFactory.java
+++ src/main/java/org/linuxprobe/luava/json/jackson/factory/ObjectMapperFactory.java
@@ -10,12 +10,14 @@ import org.linuxprobe.luava.json.jackson.deserializer.JsonDateDeserializer;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ObjectMapperFactory {
-    private static final String defaultDatePattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    private static Map<String, ObjectMapper> keyMapObjectMapper = new ConcurrentHashMap<>();
+    private static final String defaultDatePattern = "yyyy-MM-dd HH:mm:ss";
+    private static final String defaultSnakeKey = "defaultSnakeCase:";
+    private static final String defaultKey = "default:";
+    private static ConcurrentMap<String, ObjectMapper> keyMapObjectMapper = new ConcurrentHashMap<>();
 
     private static void initUniversalConfig(ObjectMapper objectMapper) {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -31,15 +33,15 @@ public class ObjectMapperFactory {
     }
 
     public static ObjectMapper getDefaultObjectMapper(String datePattern) {
-        ObjectMapper objectMapper = keyMapObjectMapper.get("default:" + datePattern);
+        ObjectMapper objectMapper = keyMapObjectMapper.get(defaultKey + datePattern);
         if (objectMapper == null) {
             synchronized ("loadDefaultObjectMapper:" + datePattern) {
-                objectMapper = keyMapObjectMapper.get("default:" + datePattern);
+                objectMapper = keyMapObjectMapper.get(defaultKey + datePattern);
                 if (objectMapper == null) {
                     objectMapper = new ObjectMapper();
                     initUniversalConfig(objectMapper);
                     objectMapper.setDateFormat(new SimpleDateFormat(datePattern));
-                    keyMapObjectMapper.put("default:" + datePattern, objectMapper);
+                    keyMapObjectMapper.put(defaultKey + datePattern, objectMapper);
                 }
             }
         }
@@ -51,16 +53,16 @@ public class ObjectMapperFactory {
     }
 
     public static ObjectMapper getDefaultSnakeCaseObjectMapper(String datePattern) {
-        ObjectMapper objectMapper = keyMapObjectMapper.get("defaultSnake:" + datePattern);
+        ObjectMapper objectMapper = keyMapObjectMapper.get(defaultSnakeKey + datePattern);
         if (objectMapper == null) {
             synchronized ("loadDefaultSnakeObjectMapper:" + datePattern) {
-                objectMapper = keyMapObjectMapper.get("defaultSnake:" + datePattern);
+                objectMapper = keyMapObjectMapper.get(defaultSnakeKey + datePattern);
                 if (objectMapper == null) {
                     objectMapper = new ObjectMapper();
                     initUniversalConfig(objectMapper);
                     objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
                     objectMapper.setDateFormat(new SimpleDateFormat(datePattern));
-                    keyMapObjectMapper.put("defaultSnake:" + datePattern, objectMapper);
+                    keyMapObjectMapper.put(defaultSnakeKey + datePattern, objectMapper);
                 }
             }
         }
